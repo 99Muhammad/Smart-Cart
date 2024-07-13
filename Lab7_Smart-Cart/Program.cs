67b14f099d8e6ca0bde218e5a5085bc7c3ee8e1a
99Muhammad <MuhammadIbraheem99m@gmail.com>
2024-07-13 22:52:09 +0300

    Add project files.

---
+++ Lab7_Smart-Cart/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab7_Smart_Cart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
